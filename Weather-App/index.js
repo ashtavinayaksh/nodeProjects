67ecaa540a01e04ef5b5f3d8f0e7d483167992a1
Ashtavinayak <ashtavinayaksharma555@gmail.com>
2021-06-20 14:45:44 +0530

    initial app file

---
+++ Weather-App/index.js
@@ -0,0 +1,40 @@
+// Backend with NodeJS
+
+const http = require('http');
+const fs = require('fs');
+var requests = require('requests');
+
+const homeFile = fs.readFileSync('home.html', 'utf-8');
+
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%loaction%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+    return temperature;
+}
+
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Chandausi&appid=d84f61f23b83c4c2fcc6c1452")
+            .on("data", (chunk) => {
+                const objdata = JSON.parse(chunk);
+                const arrData = [objdata];
+                // console.log(arrData[0].main.temp);
+                const realTimeData = arrData
+                    .map((val) => replaceVal(homeFile, val))
+                    .join("");
+                res.write(realTimeData);
+            })
+            .on('end', (err) => {
+                if (err) return console.log('connection closed due to errors', err);
+                res.end();
+            });
+    }
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log("Listening on port 8000");
+});
